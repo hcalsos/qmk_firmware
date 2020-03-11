2c94c18a7a9799b3ab5c7d5f9b4f6b767da1b7bf
Hans Christian Alsos <hcalsos@gmail.com>
2020-03-11 21:56:35 +0100

    some minor adjustments to layout2

--- keyboards/keebio/iris/keymaps/hca/keymap.c
+++ keyboards/keebio/iris/keymaps/hca/keymap.c
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSPO,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_MPLY,           LOCK,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSPC,
+     KC_LSPO,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_MPLY,           LOCK,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                     LOWER,  KC_LGUI,  KC_SPC,                    KC_SPC, ALT_ENT,  RAISE
+                                     LOWER,  KC_LGUI,  KC_SPC,                    KC_SPC, KC_RAPC,  RAISE
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
